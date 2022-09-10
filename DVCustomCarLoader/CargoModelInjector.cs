6ef60c59adf1a9349c726a93723a17622b3990e0
Niko Fox <niko@fauxnik.me>
2022-09-09 18:47:02 -0700

    remove unused `using` directives

--- DVCustomCarLoader/CargoModelInjector.cs
+++ DVCustomCarLoader/CargoModelInjector.cs
@@ -4,8 +4,6 @@ using HarmonyLib;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 
 namespace DVCustomCarLoader
