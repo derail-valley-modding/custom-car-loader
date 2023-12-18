5698a34cddeaed15d46aeb2a46327c259984bd13
Katy Fox <katycat5e@gmail.com>
2023-12-18 12:04:54 -0500

    collider validation & brake pad fixes

--- CCL.Creator/Validators/CarValidators.cs
+++ CCL.Creator/Validators/CarValidators.cs
@@ -202,15 +202,15 @@ namespace CCL.Creator.Validators
 
             // bounding collider
             var collision = collidersRoot.FindSafe(CarPartNames.COLLISION_ROOT);
-            var collisionComp = collision ? collision!.GetComponent<BoxCollider>() : null;
-            if (!(collision && collisionComp))
+            var collisionComp = collision ? collision!.GetComponentsInChildren<BoxCollider>(true) : Enumerable.Empty<Collider>();
+            if (!collision || !collisionComp.Any())
             {
                 yield return Result.Warning($"{livery.id} - Bounding {CarPartNames.COLLISION_ROOT} collider will be auto-generated");
             }
 
             // walkable
             var walkable = collidersRoot.FindSafe(CarPartNames.WALKABLE_COLLIDERS);
-            var walkableComp = walkable ? walkable!.GetComponentsInChildren<Collider>() : Enumerable.Empty<Collider>();
+            var walkableComp = walkable ? walkable!.GetComponentsInChildren<Collider>(true) : Enumerable.Empty<Collider>();
             if (!walkable || !walkableComp.Any())
             {
                 yield return Result.Failed($"{livery.id} - No {CarPartNames.WALKABLE_COLLIDERS} colliders set - car has no player collision");
@@ -218,7 +218,7 @@ namespace CCL.Creator.Validators
 
             // bogies
             var bogies = collidersRoot.FindSafe(CarPartNames.BOGIE_COLLIDERS);
-            var bogieColliders = bogies ? bogies!.GetComponentsInChildren<Collider>() : Array.Empty<Collider>();
+            var bogieColliders = bogies ? bogies!.GetComponentsInChildren<Collider>(true) : Array.Empty<Collider>();
             if (!bogies || bogieColliders.Length != 2)
             {
                 yield return Result.Failed($"{livery.id} - Incorrect number of {CarPartNames.BOGIE_COLLIDERS} colliders - should be 2");
