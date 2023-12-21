1913f51619526a619bb1ecf43d7a9f5082d34b6b
Katy Fox <katycat5e@gmail.com>
2023-12-21 16:40:38 -0500

    #175 caboose devices, buffer & interior fixes

---
+++ CCL.Importer/Proxies/CabooseDeviceReplacer.cs
@@ -0,0 +1,57 @@
+﻿using CCL.Types;
+using CCL.Types.Devices;
+using DV;
+using DV.ThingTypes;
+using System.ComponentModel.Composition;
+using UnityEngine;
+
+namespace CCL.Importer.Proxies
+{
+    [Export(typeof(IProxyReplacer))]
+    public class CabooseDeviceReplacer : IProxyReplacer
+    {
+        private static GameObject? _cabooseInterior;
+        private static GameObject CabooseInterior =>
+            Extensions.GetCached(ref _cabooseInterior, () => Globals.G.Types.TrainCarType_to_v2[TrainCarType.CabooseRed].interiorPrefab);
+
+
+        private static GameObject? _careerManager;
+        private static GameObject CareerManager =>
+            Extensions.GetCached(ref _careerManager, () => CabooseInterior.transform.Find(CarPartNames.CABOOSE_CAREER_MANAGER).gameObject);
+
+
+        private static GameObject? _remoteCharger;
+        private static GameObject RemoteCharger =>
+            Extensions.GetCached(ref _remoteCharger, () => CabooseInterior.transform.Find(CarPartNames.CABOOSE_REMOTE_CHARGER).gameObject);
+
+
+        private static GameObject? _remoteAntenna;
+        private static GameObject RemoteAntenna =>
+            Extensions.GetCached(ref _remoteAntenna, () => CabooseInterior.transform.Find(CarPartNames.CABOOSE_REMOTE_ANTENNA).gameObject);
+
+
+        public void ReplaceProxies(GameObject prefab)
+        {
+            foreach (var cmProxy in prefab.GetComponentsInChildren<CareerManagerProxy>(true))
+            {
+                var newManager = Object.Instantiate(CareerManager, cmProxy.transform, false);
+                newManager.transform.localPosition = Vector3.zero;
+                Object.Destroy(cmProxy);
+            }
+
+            foreach (var chargerProxy in prefab.GetComponentsInChildren<RemoteChargerProxy>(true))
+            {
+                var newCharger = Object.Instantiate(RemoteCharger, chargerProxy.transform, false);
+                newCharger.transform.localPosition = Vector3.zero;
+                Object.Destroy(chargerProxy);
+            }
+
+            foreach (var antennaProxy in prefab.GetComponentsInChildren<RemoteExtenderAntennaProxy>(true))
+            {
+                var newAntenna = Object.Instantiate(RemoteAntenna, antennaProxy.transform, false);
+                newAntenna.transform.localPosition = Vector3.zero;
+                Object.Destroy(antennaProxy);
+            }
+        }
+    }
+}
---
+++ CCL.Types/Devices/CareerManagerProxy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace CCL.Types.Devices
+{
+    public class CareerManagerProxy : MonoBehaviour
+    {
+        // career manager dimensions
+        private const float CM_HEIGHT = 1.765f;
+        private const float CM_BOTTOM_HEIGHT = 0.946f;
+        private const float CM_TOP_HEIGHT = 0.819f;
+        private const float CM_WIDTH = 0.722f;
+        private const float CM_DEPTH = 0.568f;
+        private const float CM_TOP_DEPTH = 0.391f;
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.white;
+            Vector3 center = new Vector3(0, CM_BOTTOM_HEIGHT / 2, 0);
+            GizmoUtil.DrawLocalPrism(transform, center, new Vector3(CM_DEPTH, CM_BOTTOM_HEIGHT, CM_WIDTH));
+
+            center = new Vector3((CM_TOP_DEPTH / 2) - (CM_DEPTH / 2), CM_HEIGHT - (CM_TOP_HEIGHT / 2), 0);
+            GizmoUtil.DrawLocalPrism(transform, center, new Vector3(CM_TOP_DEPTH, CM_TOP_HEIGHT, CM_WIDTH));
+        }
+    }
+}
