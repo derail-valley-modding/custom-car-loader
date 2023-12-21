05b275595d496e0f674a749a06b77d3179a6dbc9
Katy Fox <katycat5e@gmail.com>
2023-12-21 17:49:22 -0500

    use default brake gradient if component not found

--- CCL.Importer/PrefabWrangler.cs
+++ CCL.Importer/PrefabWrangler.cs
@@ -743,6 +743,11 @@ namespace CCL.Importer
             return newBogie;
         }
 
+        private static BrakesOverheatingColorGradient? _defaultBrakeGradient = null;
+        private static BrakesOverheatingColorGradient DefaultBrakeGradient =>
+            Extensions.GetCached(ref _defaultBrakeGradient,
+                () => Resources.FindObjectsOfTypeAll<BrakesOverheatingColorGradient>().FirstOrDefault(g => g.name == "BrakeShoeOverheatColorGradient"));
+
         private static void SetupBrakeGlows(GameObject newFab, Bogie front, Bogie rear, TrainCarLivery baseLivery)
         {
             List<Renderer> brakeRenderers = new();
@@ -785,6 +790,11 @@ namespace CCL.Importer
                 if (baseLivery.prefab.TryGetComponent(out BrakesOverheatingController baseGlow))
                 {
                     brakeGlow.overheatColor.colorGradient = baseGlow.overheatColor.colorGradient;
+                }
+                else
+                {
+                    brakeGlow.overheatColor.colorGradient = DefaultBrakeGradient.colorGradient;
+                    CCLPlugin.LogVerbose($"Apply default brake gradient to {newFab.name}");
                 }
             }
         }
