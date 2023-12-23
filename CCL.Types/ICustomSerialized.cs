da39274f0b1d4c0e1eb597b48027e0c6d12fa85d
Katy Fox <11540268+katycat5e@users.noreply.github.com>
2023-12-23 11:25:31 -0500

    add interface for custom serialization

---
+++ CCL.Types/ICustomSerialized.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCL.Types
+{
+    public interface ICustomSerialized
+    {
+        /// <summary>
+        /// Implement custom serialization of fields here (runs in editor after modifying object)
+        /// </summary>
+        void OnValidate();
+
+        /// <summary>
+        /// Implement custom deserialization of fields here
+        /// </summary>
+        void AfterImport();
+    }
+}
