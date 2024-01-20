838dd4d1a47904e9c0dbfb80b054f2ba00ef1cba
Wiz <wizardmarcos@gmail.com>
2024-01-20 21:53:55 +0000

    Merging a line.

--- CCL.Importer/Proxies/Weather/WeatherReplacer.cs
+++ CCL.Importer/Proxies/Weather/WeatherReplacer.cs
@@ -9,8 +9,7 @@ namespace CCL.Importer.Proxies.Weather
     {
         public WeatherReplacer()
         {
-            CreateMap<WindowProxy, Window>()
-                .AutoCacheAndMap()
+            CreateMap<WindowProxy, Window>().AutoCacheAndMap()
                 .ForMember(d => d.duplicates, o => o.MapFrom(s => Mapper.GetFromCache(s.duplicates)));
             CreateMap<CabinDryVolumeProxy, CabinDryVolume>().AutoCacheAndMap()
                 .ForMember(d => d.subVolumes, o => o.MapFrom(s => Mapper.GetFromCache(s.subVolumes)));
