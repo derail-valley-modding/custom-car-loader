df6dc953f05fda3b11c1be0e6ced82774099d300
Wiz <wizardmarcos@gmail.com>
2024-02-12 23:11:57 +0000

    Added tooltips to the buffer enum.

--- CCL.Types/BogieBufferTypes.cs
+++ CCL.Types/BogieBufferTypes.cs
@@ -1,4 +1,6 @@
-﻿namespace CCL.Types
+﻿using UnityEngine;
+
+namespace CCL.Types
 {
     public enum BogieType
     {
@@ -14,16 +16,27 @@
 
     public enum BufferType
     {
+        [Tooltip("Used by Boxcar")]
         Buffer02        = 400,
+        [Tooltip("Used by Stock, DE2, DH4, DM3")]
         Buffer03        = 350,
+        [Tooltip("Used by Gondola, S060")]
         Buffer04        = 550,
+        [Tooltip("Used by DE6, DE6 Slug, Microshunter")]
         Buffer05        = 40,
+        [Tooltip("Used by Boxcar Military, Refrigerator")]
         Buffer06        = 450,
+        [Tooltip("Used by Caboose, Tank, Short Tank")]
         Buffer07        = 300,
+        [Tooltip("Used by Passenger")]
         Buffer08        = 600,
+        [Tooltip("Used by Autorack, Flatbed, Flatbed Military, Flatbed Stakes, Hopper, Nuclear Flask")]
         Buffer09        = 200,
+        [Tooltip("Used by S282A (only front side)")]
         S282A           = 20,
+        [Tooltip("Used by S282B (only rear side)")]
         S282B           = 21,
+        [Tooltip("Use your own custom bogie")]
         Custom          = 10000
     }
 }
