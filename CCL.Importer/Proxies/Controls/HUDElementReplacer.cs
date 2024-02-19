e73796b8a6cbfaa108a1db62e3950ed99a5becce
Wiz <wizardmarcos@gmail.com>
2024-02-19 23:53:15 +0000

    Adds components that allow renaming HUD controls.

---
+++ CCL.Importer/Proxies/Controls/HUDElementReplacer.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CCL.Types.Proxies.Controls;
+using DV.HUD;
+
+namespace CCL.Importer.Proxies.Controls
+{
+    internal class HUDElementReplacer : Profile
+    {
+        public HUDElementReplacer()
+        {
+            CreateMap<NotchedControlCustomNamesProxy, NotchedControlCustomNames>().AutoCacheAndMap();
+            CreateMap<NotchedControlNumberedNamesProxy, NotchedControlNumberedNames>().AutoCacheAndMap();
+            CreateMap<PercentageControlNamesProxy, PercentageControlNames>().AutoCacheAndMap();
+        }
+    }
+}
