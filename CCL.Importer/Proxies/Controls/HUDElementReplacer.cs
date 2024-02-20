64f2050af1dc391eaa20a442dd644f6dc5ba04ec
Wiz <wizardmarcos@gmail.com>
2024-02-20 15:40:05 +0000

    Added control readers.

--- CCL.Importer/Proxies/Controls/HUDElementReplacer.cs
+++ CCL.Importer/Proxies/Controls/HUDElementReplacer.cs
@@ -11,6 +11,12 @@ namespace CCL.Importer.Proxies.Controls
             CreateMap<NotchedControlCustomNamesProxy, NotchedControlCustomNames>().AutoCacheAndMap();
             CreateMap<NotchedControlNumberedNamesProxy, NotchedControlNumberedNames>().AutoCacheAndMap();
             CreateMap<PercentageControlNamesProxy, PercentageControlNames>().AutoCacheAndMap();
+
+            CreateMap<LocoControlsReaderProxy, LocoControlsReader>().AutoCacheAndMap();
+            CreateMap<LocoIndicatorReaderProxy, LocoIndicatorReader>().AutoCacheAndMap()
+                .ForAllMembers(o => o.MapFrom(m => Mapper.GetFromCache(m)));
+            CreateMap<LocoLampReaderProxy, LocoLampReader>().AutoCacheAndMap()
+                .ForAllMembers(o => o.MapFrom(m => Mapper.GetFromCache(m)));
         }
     }
 }
