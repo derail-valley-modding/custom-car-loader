fbce95411f20e1753d51127e17baac867338a9ef
Katy Fox <katycat5e@gmail.com>
2024-02-21 14:32:46 -0500

    #233 hud fuse references, add interior control manager

---
+++ CCL.Types/Proxies/Controls/LocoFuseBoxReferenceProxy.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace CCL.Types.Proxies.Controls
+{
+    public class LocoFuseBoxReferenceProxy : MonoBehaviour
+    {
+        public GameObject starterFuse = null!;
+        public GameObject electricsFuse = null!;
+        public GameObject tractionMotorFuse = null!;
+        public GameObject starterControl = null!;
+    }
+}
