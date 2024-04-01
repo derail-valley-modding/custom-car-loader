23fcbca04ed2c71482561f8e7ea0772661c2528b
Wiz <wizardmarcos@gmail.com>
2024-04-01 01:50:11 +0100

    Added keyboard controls.

---
+++ CCL.Types/Proxies/Controls/AKeyboardInputProxy.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace CCL.Types.Proxies.Controls
+{
+    public abstract class AKeyboardInputProxy : MonoBehaviour { }
+
+    public class ButtonUseKeyboardInputProxy : AKeyboardInputProxy
+    {
+        public BaseKeyType useKey;
+    }
+    
+    public class FireboxKeyboardInputProxy : AKeyboardInputProxy
+    {
+        public BaseKeyType lightFireKey = BaseKeyType.LightFire;
+        public BaseKeyType shovelCoalKey = BaseKeyType.Shovel;
+    }
+    
+    public class MouseScrollKeyboardInputProxy : AKeyboardInputProxy
+    {
+        public BaseKeyType scrollUpKey;
+        public BaseKeyType scrollDownKey;
+    }
+    
+    public class NotchedPortKeyboardInputProxy : AKeyboardInputProxy
+    {
+        public BaseKeyType positiveApplyKey;
+        public BaseKeyType negativeApplyKey;
+    }
+    
+    public class PhysicsForceKeyboardInputProxy : AKeyboardInputProxy
+    {
+        public BaseKeyType positiveApplyKey;
+        public BaseKeyType negativeApplyKey;
+    }
+    
+    public class PhysicsTorqueKeyboardInputProxy : AKeyboardInputProxy
+    {
+        public BaseKeyType positiveApplyKey;
+        public BaseKeyType negativeApplyKey;
+    }
+
+    public class ToggleSwitchUseKeyboardInputProxy : AKeyboardInputProxy
+    {
+        public BaseKeyType useKey;
+    }
+
+    public class ToggleValueKeyboardInputProxy : AKeyboardInputProxy
+    {
+        public BaseKeyType toggleKey;
+    }
+}
