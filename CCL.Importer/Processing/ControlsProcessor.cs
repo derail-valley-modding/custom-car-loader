5c1ae69d9fbbda70a7ce1ccc7efadfb96ef77e14
Wiz <wizardmarcos@gmail.com>
2024-04-02 17:45:06 +0100

    Automatically add the control manager as an import step.

---
+++ CCL.Importer/Processing/ControlsProcessor.cs
@@ -0,0 +1,25 @@
+﻿using DV.CabControls;
+using DV.KeyboardInput;
+using System.ComponentModel.Composition;
+
+namespace CCL.Importer.Processing
+{
+    [Export(typeof(IModelProcessorStep))]
+    [RequiresStep(typeof(ProxyScriptProcessor))]
+    internal class ControlsProcessor : ModelProcessorStep
+    {
+        public override void ExecuteStep(ModelProcessor context)
+        {
+            foreach (var item in context.Car.AllPrefabs)
+            {
+                var keyboardControls = item.GetComponentsInChildren<AKeyboardInput>();
+
+                if (keyboardControls.Length > 0)
+                {
+                    var controller = item.AddComponent<InteractablesKeyboardControl>();
+                    controller.OnValidate();
+                }
+            }
+        }
+    }
+}
