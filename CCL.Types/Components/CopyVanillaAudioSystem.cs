103059247bdcd4a856b2e60b4ae61bfd0cd9ab0c
Wiz <wizardmarcos@gmail.com>
2024-04-08 15:45:26 +0100

    Added copy component for audio.

---
+++ CCL.Types/Components/CopyVanillaAudioSystem.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace CCL.Types.Components
+{
+    public class CopyVanillaAudioSystem : MonoBehaviour
+    {
+        public VanillaAudioSystem AudioSystem;
+    }
+
+    public enum VanillaAudioSystem
+    {
+        FullDE2Audio,
+        FullDE6Audio,
+        FullDH4Audio,
+        FullDM3Audio,
+        FullS060Audio,
+        FullS282Audio,
+        FullMicroshunterAudio,
+
+        SteamerCoalDump = 200,
+        SteamerFire,
+        SteamerFireboxWind,
+        SteamerSand,
+        SteamerSafetyRelease,
+        SteamerBlowdown,
+        SteamerChestAdmission,
+        SteamerCylinderCrack,
+        SteamerCylinderCock,
+        SteamerChuff,
+        SteamerInjector,
+        SteamerValveGear,
+        SteamerCrownSheet,
+        SteamerAirPump,
+        SteamerDynamo,
+        SteamerBellRing,
+        SteamerBellPump
+    }
+}
