4263f7a1852cd72367298503d902c1a448b3af36
Wiz <wizardmarcos@gmail.com>
2024-12-22 00:15:32 +0000

    Updated loco spawning.

--- CCL.Importer/Patches/StationLocoSpawnerPatches.cs
+++ CCL.Importer/Patches/StationLocoSpawnerPatches.cs
@@ -13,6 +13,7 @@ namespace CCL.Importer.Patches
         private static void StartPostfix(StationLocoSpawner __instance)
         {
             CCLPlugin.Log($"Finding loco spawn groups to inject into '{__instance.name}'");
+            CCLPlugin.LogVerbose($"Track length: {__instance.locoSpawnTrack.logicTrack.length}m");
 
             // Get the groups from the liveries.
             foreach (var car in CarManager.CustomCarTypes)
@@ -24,7 +25,7 @@ namespace CCL.Importer.Patches
                         // If the group is supposed to use this spawner...
                         if (group.Track.ToName() == __instance.name)
                         {
-                            CCLPlugin.Log($"Injecting loco spawn group [{variant.id}, {string.Join(", ", group.AdditionalLiveries)}]");
+                            CCLPlugin.LogVerbose($"Injecting loco spawn group [{variant.id}, {string.Join(", ", group.AdditionalLiveries)}]");
                             __instance.locoTypeGroupsToSpawn.Add(FromGroup(variant, group));
                         }
                     }
@@ -34,47 +35,48 @@ namespace CCL.Importer.Patches
             // Randomise spawn index again so it doesn't always spawn vanilla the first time.
             __instance.nextLocoGroupSpawnIndex = Random.Range(0, __instance.locoTypeGroupsToSpawn.Count);
 
-            // Calculate spawn chances for this spawner to use in the catalog (for vanilla).
-            if (TryToVanillaStationId(__instance.name, out string id))
+            // Calculate spawn chances for this spawner to use in the catalog.
+            // If the ID is not from a vanilla station, skip.
+            if (!TryToVanillaStationId(__instance.name, out string id)) return;
+
+            // Get the station chances instead of spawner chances.
+            if (!CatalogGenerator.NotSpawnChances.TryGetValue(id, out var chances))
             {
-                // Get the station chances instead of spawner chances.
-                if (!CatalogGenerator.SpawnChances.TryGetValue(id, out var chances))
-                {
-                    chances = new();
-                    CatalogGenerator.SpawnChances.Add(id, chances);
-                }
+                chances = new();
+                CatalogGenerator.NotSpawnChances.Add(id, chances);
+            }
 
-                // Get how many groups each car type is in.
-                Dictionary<string, int> groupCounts = new();
+            // Get how many spawn groups each car type is in.
+            Dictionary<string, int> groupCounts = new();
 
-                foreach (var group in __instance.locoTypeGroupsToSpawn)
+            foreach (var group in __instance.locoTypeGroupsToSpawn)
+            {
+                foreach (var livery in group.liveries)
                 {
-                    foreach (var livery in group.liveries)
+                    string parentId = livery.parentType.id;
+
+                    // Add a count if it doesn't exist.
+                    if (!groupCounts.ContainsKey(parentId))
                     {
-                        string parentId = livery.parentType.id;
-
-                        if (!groupCounts.ContainsKey(parentId))
-                        {
-                            groupCounts.Add(parentId, 1);
-                        }
-                        else
-                        {
-                            groupCounts[parentId]++;
-                        }
+                        groupCounts.Add(parentId, 1);
+                    }
+                    else
+                    {
+                        groupCounts[parentId]++;
                     }
                 }
+            }
 
-                foreach (var count in groupCounts)
+            foreach (var count in groupCounts)
+            {
+                // Add the chance for this type if it doesn't yet exist.
+                if (!chances.ContainsKey(count.Key))
                 {
-                    // Add the chance for this type if it doesn't yet exist.
-                    if (!chances.ContainsKey(count.Key))
-                    {
-                        chances.Add(count.Key, 0);
-                    }
-
-                    // Actual chance is the maximum of all chances at each spawner of the station.
-                    chances[count.Key] = Mathf.Max(chances[count.Key], (float)count.Value / __instance.locoTypeGroupsToSpawn.Count);
+                    chances.Add(count.Key, 0);
                 }
+
+                // Apply the chance to NOT spawn.
+                chances[count.Key] *= 1.0f - ((float)count.Value / __instance.locoTypeGroupsToSpawn.Count);
             }
         }
 
