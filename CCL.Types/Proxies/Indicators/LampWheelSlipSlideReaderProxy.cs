c81a01b194cde87ae3b893b7980be1c50b157c98
Wiz <wizardmarcos@gmail.com>
2025-01-19 04:25:14 +0000

    Added LampWheelSlipSlideReaderProxy.

---
+++ CCL.Types/Proxies/Indicators/LampWheelSlipSlideReaderProxy.cs
@@ -0,0 +1,24 @@
+﻿using CCL.Types.Proxies.Ports;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CCL.Types.Proxies.Indicators
+{
+    public class LampWheelSlipSlideReaderProxy : MonoBehaviour, IHasFuseIdFields
+    {
+        public enum WheelslipDetectionMode
+        {
+            Individual,
+            MultipleUnit
+        }
+
+        public WheelslipDetectionMode wheelslipDetectionMode;
+        [FuseId]
+        public string fuseId;
+
+        public IEnumerable<FuseIdField> ExposedFuseIdFields => new[]
+        {
+            new FuseIdField(this, nameof(fuseId), fuseId),
+        };
+    }
+}
