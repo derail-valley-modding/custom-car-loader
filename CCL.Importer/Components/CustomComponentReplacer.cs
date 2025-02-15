37f745206fa6a5ce2a95956a8a671a9e7c702e3a
Wiz <wizardmarcos@gmail.com>
2025-02-15 17:10:30 +0000

    Added IndicatorShaderValueProxy. Added IndicatorShaderCustomValue.

--- CCL.Importer/Components/CustomComponentReplacer.cs
+++ CCL.Importer/Components/CustomComponentReplacer.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using CCL.Importer.Components.Headlights;
+using CCL.Importer.Components.Indicators;
 using CCL.Types.Components;
 using CCL.Types.Components.Headlights;
+using CCL.Types.Components.Indicators;
 
 namespace CCL.Importer.Components
 {
@@ -9,18 +11,28 @@ namespace CCL.Importer.Components
     {
         public CustomComponentReplacer()
         {
+            MapCoupling();
+            MapHeadlights();
+            MapIndicators();
+        }
+
+        private void MapCoupling()
+        {
             CreateMap<CarAutoCoupler, CarAutoCouplerInternal>().AutoCacheAndMap();
             CreateMap<RigidCoupler, RigidCouplerInternal>().AutoCacheAndMap();
             CreateMap<VirtualHandbrakeOverrider, VirtualHandbrakeOverriderInternal>().AutoCacheAndMap();
             CreateMap<DuplicateHandbrakeOverrider, DuplicateHandbrakeOverriderInternal>().AutoCacheAndMap();
-
-            MapHeadlights();
         }
 
         private void MapHeadlights()
         {
             CreateMap<FrontConnectedDualCarAutomaticHeadlightsController, FrontConnectedDualCarAutomaticHeadlightsControllerInternal>().AutoCacheAndMap();
             CreateMap<FrontAndRearConnectedTriCarAutomaticHeadlightsController, FrontAndRearConnectedTriCarAutomaticHeadlightsControllerInternal>().AutoCacheAndMap();
+        }
+
+        private void MapIndicators()
+        {
+            CreateMap<IndicatorShaderCustomValue, IndicatorShaderCustomValueInternal>().AutoCacheAndMap();
         }
     }
 }
---
+++ CCL.Types/Components/Indicators/IndicatorShaderCustomValue.cs
@@ -0,0 +1,11 @@
+﻿using CCL.Types.Proxies.Indicators;
+using UnityEngine;
+
+namespace CCL.Types.Components.Indicators
+{
+    public class IndicatorShaderCustomValue : IndicatorProxy
+    {
+        public MeshRenderer Renderer = null!;
+        public string PropertyId = string.Empty;
+    }
+}
