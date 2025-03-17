ab6b7b26059b9e91ac1ffbe4234a2fb3b86221b8
Wiz <wizardmarcos@gmail.com>
2025-03-17 00:10:23 +0000

    Added other model support to oiling points. Improved oiling point gizmo.

--- CCL.Types/Components/ManualOilingPoint.cs
+++ CCL.Types/Components/ManualOilingPoint.cs
@@ -4,15 +4,36 @@ namespace CCL.Types.Components
 {
     public class ManualOilingPoint : MonoBehaviour
     {
-        private static Vector3 s_pos = Vector3.up * 0.05f;
-        private static Vector3 s_size = Vector3.up * 0.1f;
+        public enum Model
+        {
+            CupOnly,
+            CupAndPipe,
+            CupWallAndPipe
+        }
+
+        private static Vector3 s_pos = new Vector3(0.00f, 0.02f, 0.00f);
+        private static Vector3 s_size = new Vector3(0.05f, 0.05f, 0.05f);
+        private static Vector3 s_posPlate = new Vector3(0.02f, -0.02f, 0.00f);
+        private static Vector3 s_sizePlate = new Vector3(0.08f, 0.01f, 0.04f);
+        private static Vector3 s_posWall = new Vector3(0.03f, 0.02f, 0.00f);
+        private static Vector3 s_sizeWall = new Vector3(0.01f, 0.03f, 0.08f);
 
         public string SyncTag = "o0";
+        public Model CupModel = Model.CupOnly;
 
         private void OnDrawGizmos()
         {
             Gizmos.matrix = transform.localToWorldMatrix;
             Gizmos.DrawCube(s_pos, s_size);
+
+            if (CupModel == Model.CupAndPipe)
+            {
+                Gizmos.DrawCube(s_posPlate, s_sizePlate);
+            }
+            else if (CupModel == Model.CupWallAndPipe)
+            {
+                Gizmos.DrawCube(s_posWall, s_sizeWall);
+            }
         }
     }
 }
