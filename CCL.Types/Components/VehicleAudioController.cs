8aff6f7f0453e77cf01a350a1bc735edc93515f4
Wiz <wizardmarcos@gmail.com>
2025-04-15 04:10:04 +0100

    Added ability to change the position of certain audios.

---
+++ CCL.Types/Components/VehicleAudioController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace CCL.Types.Components
+{
+    public class VehicleAudioController : MonoBehaviour
+    {
+        public Transform? CustomWheelAudioPosition1;
+        public Transform? CustomWheelAudioPosition2;
+
+        public Transform? BrakeCylinderExhaustPosition;
+        public Transform? BrakeAirflowPosition;
+    }
+}
