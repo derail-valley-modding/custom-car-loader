60f263b5573aa01e59fb420a2bcbe6d08c30ab58
Wiz <wizardmarcos@gmail.com>
2025-04-19 03:05:51 +0100

    Added cab rendering components.

---
+++ CCL.Types/Proxies/CabinRenderOrderingProxy.cs
@@ -0,0 +1,34 @@
+﻿using CCL.Types.Json;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace CCL.Types.Proxies
+{
+    public class CabinRenderOrderingProxy : MonoBehaviour, ICustomSerialized
+    {
+        [Serializable]
+        public class OrderingRenderer
+        {
+            public SortingGroup group = null!;
+            public short initialOrder;
+            public short whenInside;
+        }
+
+        public CameraTriggerProxy triggerNullable = null!;
+        public List<OrderingRenderer> ordering = new List<OrderingRenderer>();
+
+        private string? _json;
+
+        public void OnValidate()
+        {
+            _json = JSONObject.ToJson(ordering);
+        }
+
+        public void AfterImport()
+        {
+            ordering = JSONObject.FromJson(_json, () => new List<OrderingRenderer>());
+        }
+    }
+}
