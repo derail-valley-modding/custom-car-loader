f2a66fda957a15b212afa239ef90313e5a99c7a1
Wiz <wizardmarcos@gmail.com>
2025-08-25 00:35:04 +0100

    Changed spawn chance station matching to use regex.

--- CCL.Importer/Patches/StationLocoSpawnerPatches.cs
+++ CCL.Importer/Patches/StationLocoSpawnerPatches.cs
@@ -4,6 +4,7 @@ using HarmonyLib;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 namespace CCL.Importer.Patches
@@ -11,6 +12,8 @@ namespace CCL.Importer.Patches
     [HarmonyPatch(typeof(StationLocoSpawner))]
     internal class StationLocoSpawnerPatches
     {
+        private static Regex s_regex = new("\\[Y\\]_\\[([a-z0-9]*)\\]_\\[(.*)]", RegexOptions.IgnoreCase);
+
         [HarmonyPostfix, HarmonyPatch(nameof(StationLocoSpawner.Start))]
         private static void StartPostfix(StationLocoSpawner __instance)
         {
@@ -37,6 +40,7 @@ namespace CCL.Importer.Patches
                             {
                                 CCLPlugin.LogVerbose($"Injecting loco spawn group [{variant.id}]");
                             }
+
                             __instance.locoTypeGroupsToSpawn.Add(FromGroup(variant, group));
                         }
                     }
@@ -48,7 +52,8 @@ namespace CCL.Importer.Patches
 
             // Calculate spawn chances for this spawner to use in the catalog.
             // If the ID is not from a vanilla station, skip.
-            if (!TryToVanillaStationId(__instance.name, out string id)) return;
+            //if (!TryToVanillaStationId(__instance.name, out string id)) return;
+            if (!TryMatchName(__instance.locoSpawnTrackName, out string id)) return;
 
             //var id = RailTrackRegistry.RailTrackToLogicTrack[__instance.locoSpawnTrack].ID.yardId;
             StationSpawnChanceData.AddData(id, __instance);
@@ -57,6 +62,20 @@ namespace CCL.Importer.Patches
         private static bool TryToVanillaStationId(string name, out string id)
         {
             return LocoSpawnGroup.SpawnerNameToId.TryGetValue(name, out id);
+        }
+
+        private static bool TryMatchName(string name, out string id)
+        {
+            var match = s_regex.Match(name);
+
+            if (match.Success)
+            {
+                id = match.Groups[1].Value;
+                return true;
+            }
+
+            id = string.Empty;
+            return false;
         }
 
         private static ListTrainCarTypeWrapper FromGroup(TrainCarLivery variant, LocoSpawnGroup group)
