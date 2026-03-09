5330ddd1a0fa4734d9d2bd3881099c1de2a808cf
Wiz <wizardmarcos@gmail.com>
2026-03-09 22:25:49 +0000

    Added new field to Indicator Port Reader. Improved validation.

--- CCL.Creator/Validators/ComponentValidator.cs
+++ CCL.Creator/Validators/ComponentValidator.cs
@@ -9,6 +9,25 @@ namespace CCL.Creator.Validators
     {
         public override string TestName => "Components";
 
+        public override ValidationResult Validate(CustomCarType car)
+        {
+            var result = base.Validate(car);
+
+            if (result.Status == ResultStatus.Critical) return result;
+
+            var tested = result.Status != ResultStatus.Skipped;
+            int count = 0;
+
+            foreach (var prefab in car.AllPrefabs)
+            {
+                count += CheckPrefab(prefab, car.id, result);
+
+                if (result.Status == ResultStatus.Critical) return result;
+            }
+
+            return (tested || count > 0) ? result : Skip();
+        }
+
         protected override ValidationResult ValidateLivery(CustomCarVariant livery)
         {
             int count = 0;
@@ -16,37 +35,46 @@ namespace CCL.Creator.Validators
 
             foreach (var prefab in livery.AllPrefabs)
             {
-                var components = prefab.GetComponentsInChildren<ISelfValidation>();
+                count += CheckPrefab(prefab, livery.id, result);
 
-                count += components.Length;
-
-                foreach (var comp in components)
-                {
-                    if (!(comp is Component self)) continue;
-
-                    switch (comp.Validate(out var message))
-                    {
-                        case SelfValidationResult.Info:
-                            result.AddInformation($"{livery.id} - {message}");
-                            break;
-                        case SelfValidationResult.Warning:
-                            result.Warning(AddCompToMessage(self, message), self);
-                            break;
-                        case SelfValidationResult.Fail:
-                            result.Fail(AddCompToMessage(self, message), self);
-                            break;
-                        case SelfValidationResult.Critical:
-                            result.CriticalFail(AddCompToMessage(self, message), self);
-                            return result;
-                        default:
-                            continue;
-                    }
-                }
+                if (result.Status == ResultStatus.Critical) return result;
             }
 
             count += TestLubricatorRatchet(livery, result);
 
             return count == 0 ? Skip() : result;
+        }
+
+        private static int CheckPrefab(GameObject prefab, string id, ValidationResult result)
+        {
+            var components = prefab.GetComponentsInChildren<ISelfValidation>();
+            var count = components.Length;
+
+            foreach (var comp in components)
+            {
+                if (!(comp is Component self)) continue;
+
+                switch (comp.Validate(out var message))
+                {
+                    case SelfValidationResult.Info:
+                        result.AddInformation($"{id} - {message}");
+                        continue;
+                    case SelfValidationResult.Warning:
+                        result.Warning(AddCompToMessage(self, message), self);
+                        continue;
+                    case SelfValidationResult.Fail:
+                        result.Fail(AddCompToMessage(self, message), self);
+                        continue;
+                    case SelfValidationResult.Critical:
+                        result.CriticalFail(AddCompToMessage(self, message), self);
+                        return count;
+                    default:
+                        Debug.LogWarning($"Unknown self validation result from {self.name}/{self.GetType().Name}");
+                        continue;
+                }
+            }
+
+            return count;
 
             static string AddCompToMessage(Component comp, string message)
             {
@@ -54,7 +82,7 @@ namespace CCL.Creator.Validators
             }
         }
 
-        private int TestLubricatorRatchet(CustomCarVariant livery, ValidationResult result)
+        private static int TestLubricatorRatchet(CustomCarVariant livery, ValidationResult result)
         {
             var comps = livery.AllPrefabs.GetComponentsInChildren<LubricatorRatchetProxy>();
 
